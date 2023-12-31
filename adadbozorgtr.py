269669c511260430d0600a7277ac218068ba14fe
Erfan Mehrban <112585242+ErfMehrb@users.noreply.github.com>
2024-01-01 00:13:18 +0330

    Add files via upload

---
+++ adadbozorgtr.py
@@ -0,0 +1,7 @@
+firstNumber = input('give me a first number')
+secondNumber = input('give me a second number')
+
+if int(firstNumber) >= int(secondNumber):
+    print(firstNumber)
+else:
+  print(secondNumber)
